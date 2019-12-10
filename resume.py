e014761f2b7cdf96d57a0d384c178dcbaf84171b
Matt Wittmann <mcwitt@gmail.com>
2019-12-09 16:58:33 -0800

    Make format_date safer; add Fire CLI

--- resume.py
+++ resume.py
@@ -1,34 +1,34 @@
+import fire
 import yaml
 import jinja2
 import os
 
 
 def format_date(date, fmt='%Y-%m-%d'):
-    try:
-        return date.strftime(fmt)
-    except AttributeError:
-        return date
+    return date if date == 'present' else date.strftime(fmt)
+
+def resume(data, template):
+    with open('resume.yaml') as f:
+        r = yaml.load(f)
+
+    latex_env = jinja2.Environment(
+        block_start_string='\BLOCK{',
+        block_end_string='}',
+        variable_start_string='\VAR{',
+        variable_end_string='}',
+        comment_start_string='\#{',
+        comment_end_string='}',
+        line_statement_prefix='%%',
+        line_comment_prefix='%#',
+        trim_blocks=True,
+        autoescape=False,
+        loader=jinja2.FileSystemLoader(os.path.abspath('.'))
+       )
+
+    latex_env.filters['format_date'] = format_date
+    template = latex_env.get_template('resume-template.tex')
+    print(template.render(**r))
 
 
-with open('resume.yaml') as f:
-    r = yaml.load(f)
-
-latex_env = jinja2.Environment(
-    block_start_string='\BLOCK{',
-    block_end_string='}',
-    variable_start_string='\VAR{',
-    variable_end_string='}',
-    comment_start_string='\#{',
-    comment_end_string='}',
-    line_statement_prefix='%%',
-    line_comment_prefix='%#',
-    trim_blocks=True,
-    autoescape=False,
-    loader=jinja2.FileSystemLoader(os.path.abspath('.'))
-)
-
-latex_env.filters['format_date'] = format_date
-
-template = latex_env.get_template('resume-template.tex')
-
-print(template.render(**r))
+if __name__ == '__main__':
+    fire.Fire(resume)
